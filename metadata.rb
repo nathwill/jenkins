40a10258731e0b8a92b34f74e07f8e15b4877cb7
Nathan Williams <nath.e.will@gmail.com>
2018-01-08 10:29:07 -0800

    bump ver

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.6.1'
+version          '2.6.2'
 
 source_url 'https://github.com/chef-cookbooks/jenkins'
 issues_url 'https://github.com/chef-cookbooks/jenkins/issues'
